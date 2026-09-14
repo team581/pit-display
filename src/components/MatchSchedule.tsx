108683c657ff1b5fa09374c0f54a4177594199fa
Jonah Snider <jonah@jonahsnider.com>
2026-09-13 23:08:47 -0700

    Change "Starts at" to "Starts"
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/components/MatchSchedule.tsx
+++ src/components/MatchSchedule.tsx
@@ -19,7 +19,7 @@ export function MatchSchedule({ matches, now }: { matches: Dashboard['upcomingMa
 						<strong {...stylex.props(styles.matchNumber)}>{match.displayLabel}</strong>
 						<div {...stylex.props(styles.matchTime)}>
 							<strong {...stylex.props(styles.startTime)}>
-								Starts at {formatClock(match.scheduledTime)} ({formatRelativeTime(match.startTime, now, '')})
+								Starts {formatClock(match.scheduledTime)} ({formatRelativeTime(match.startTime, now, '')})
 							</strong>
 						</div>
 						<StatusPill match={match} />
